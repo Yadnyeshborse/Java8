a5dc05933a8894438e32a97a84a113dc67a69b53
Yadnyesh Borse <yadvborse27@gmail.com>
2025-06-26 00:04:51 +0530

    Updated Code

---
+++ src/GroupingByExample/MaleFemaleavg.java
@@ -0,0 +1,22 @@
+package GroupingByExample;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MaleFemaleavg {
+
+    public static void main(String[] args) {
+        MaleFemale m1=new MaleFemale("Raj",75,"yadvbors27@gmail.com","M");
+        MaleFemale m2=new MaleFemale("Ravan",56,"rajbors27@gmail.com","F");
+        MaleFemale m3=new MaleFemale("Ram",95,"opvbors27@gmail.com","M");
+        MaleFemale m4=new MaleFemale("Raksha",35,"sanors27@gmail.com","F");
+
+        List<MaleFemale> list= Arrays.asList(m1,m2,m3,m4);
+
+        Map<String, Double> avgByGender =list.stream()
+                .collect(Collectors.groupingBy(MaleFemale::getGender,Collectors.averagingInt(MaleFemale::getAge)));
+        System.out.println(avgByGender);
+    }
+}
---
+++ src/Questions/InterviewQuestions/Product.java
@@ -0,0 +1,61 @@
+package Questions.InterviewQuestions;
+
+
+public class Product {
+    Integer id;
+    String name;
+    Double price;
+    String category;
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                '}';
+    }
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, Double price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
