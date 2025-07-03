557084f33080f6622acc82017e4831da352b17cd
Yadnyesh Borse <yadvborse27@gmail.com>
2025-07-03 09:51:26 +0530

    Updated Code

---
+++ src/Questions/InterviewQuestions/CharsDemo.java
@@ -0,0 +1,16 @@
+package Questions.InterviewQuestions;
+
+import java.util.stream.IntStream;
+
+public class CharsDemo {
+    public static void main(String[] args) {
+        "abstribgz".chars().forEach(System.out::println);
+        //.chars() is a method available on String objects that returns an
+        // IntStream of Unicode code points of the characters in the string.
+
+
+        //Since .chars() gives int, you often convert it back to char like this:
+        "strings".chars().mapToObj(i->(char) i).forEach(System.out::println);
+
+    }
+}
