8c0e44b2ae299ff25e743a17968d4043c995bf2e
Yadnyesh Borse <yadvborse27@gmail.com>
2025-12-25 21:09:14 +0530

    added new file

---
+++ src/Questions/interviewQuestions/Method_refer.java
@@ -0,0 +1,10 @@
+package Questions.interviewQuestions;
+
+import java.util.stream.IntStream;
+
+public class Method_refer {
+    public static void main(String[] args) {
+        IntStream.range(0,10).forEach(System.out::println);
+
+    }
+}
