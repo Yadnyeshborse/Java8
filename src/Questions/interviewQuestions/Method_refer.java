dc11ba2c7761bc745c2cf9426b38cc018b1d3282
Yadnyesh Borse <yadvborse27@gmail.com>
2025-12-25 21:10:22 +0530

    added new file v

--- src/Questions/interviewQuestions/Method_refer.java
+++ src/Questions/interviewQuestions/Method_refer.java
@@ -4,7 +4,9 @@ import java.util.stream.IntStream;
 
 public class Method_refer {
     public static void main(String[] args) {
-        IntStream.range(0,10).forEach(System.out::println);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
 
     }
 }
