2d3a6ad6cd5e45768c5a434e8280e69456b464a4
Yadnyesh Borse <yadvborse27@gmail.com>
2025-12-25 20:21:22 +0530

    updated

--- src/Questions/interviewQuestions/B_Demo.java
+++ src/Questions/interviewQuestions/B_Demo.java
@@ -1,18 +1,15 @@
 package Questions.interviewQuestions;
 
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class B_Demo {
     public static void main(String[] args) {
 
-        IntStream.range(3,10).filter(i->isBoxed(i)).forEach(System.out::println);
+        IntStream.range(3,10).filter(B_Demo::isBoxed).forEach(System.out::println);
     }
 
     private static boolean isBoxed(int i) {
         return  IntStream.range(2, (int) Math.sqrt(i)).anyMatch(o->i%o==0);
     }
-
+//comment
 }
