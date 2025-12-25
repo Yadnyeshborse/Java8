ab782bc5130b2b1dc71d47cb95462d1024b3d517
Yadnyesh Borse <yadvborse27@gmail.com>
2025-12-26 00:49:39 +0530

    Updated Method_refer.java

--- src/Questions/interviewQuestions/Method_refer.java
+++ src/Questions/interviewQuestions/Method_refer.java
@@ -1,11 +1,14 @@
 package Questions.interviewQuestions;
 
-import java.util.stream.IntStream;
+
 
 public class Method_refer {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+        }
+        for (int i = 0; i <20 ; i++) {
+
         }
 
     }
