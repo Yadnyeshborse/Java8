3cb6cd9538849a7e08f86b61a36bba43f5715d7b
Yadnyesh Borse <yadvborse27@gmail.com>
2025-12-25 20:27:43 +0530

    added file

--- src/Questions/interviewQuestions/Abc.java
+++ src/Questions/interviewQuestions/Abc.java
@@ -2,7 +2,7 @@ package Questions.interviewQuestions;
 
 public class Abc {
     public static void main(String[] args) {
-        int arr[]={1,2,3,4};
+        int arr[]={1,2,3,4,9};
         for (int i = 0; i <arr.length ; i++) {
             System.out.println(i);
         }
