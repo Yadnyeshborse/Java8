b3e7d589da581725913b7c68377829d0a92add17
Yadnyesh Borse <yadvborse27@gmail.com>
2025-12-26 20:16:10 +0530

    Deleted

--- src/Questions/interviewQuestions/Commit_exercise.java
+++
@@ -1,9 +0,0 @@
-package Questions.interviewQuestions;
-
-public class Commit_exercise {
-    public static void main(String[] args) {
-        String name="Saurab";
-
-
-    }
-}
--- src/Questions/interviewQuestions/Commit_history.java
+++
@@ -1,7 +0,0 @@
-package Questions.interviewQuestions;
-
-public class Commit_history {
-    public static void main(String[] args) {
-        int a=30;
-    }
-}
